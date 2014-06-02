51536f8fde5911efc2dc7b4f8c0ad8ee8603e70a
Brian Kovacs <brian.kovacs@me.com>
2014-06-02 11:45:41 -0400

    Created health pack prefab

---
+++ Luminos Void/Assets/Scripts/HealthPackScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPackScript : MonoBehaviour 
+{
+	public int replenishHealth = 1;
+	private int maxHealth;
+
+	// Use this for initialization
+	void Start () 
+	{
+		maxHealth = GameObject.Find("Player").GetComponent<HealthScript>().hp;
+	}
+	
+	// Update is called once per frame
+	void Update () { }
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		PlayerScript player = otherCollider.GetComponent<PlayerScript>();
+		HealthScript health = otherCollider.GetComponent<HealthScript>();
+
+		if (player != null)
+		{
+			if (health.hp < maxHealth)
+			{
+				health.hp += replenishHealth;
+				if(health.hp > maxHealth)
+				{
+					health.hp = maxHealth;
+				}
+			}
+			Destroy(this.gameObject);
+		}
+	}
+}
