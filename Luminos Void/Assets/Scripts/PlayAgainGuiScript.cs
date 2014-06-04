5d9300fad37fc1b3388cfa7516244e7d80a3fc7c
Brian Kovacs <brian.kovacs@me.com>
2014-06-04 18:12:35 -0400

    Added Start and Play Again scenes

---
+++ Luminos Void/Assets/Scripts/PlayAgainGuiScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAgainGuiScript : MonoBehaviour 
+{
+	public GUISkin myGuiSkin;
+	public float scaleX = 0.5f;
+	public float scaleY = 0.5f;
+
+	void OnGUI()
+	{
+		GUI.skin = myGuiSkin;
+		
+		const int buttonWidth = 500;
+		const int buttonHeight = 100;
+
+		if (
+			GUI.Button(
+			new Rect(
+			((float)Screen.width * scaleX) - (buttonWidth / 2),
+			((float)Screen.height * scaleY) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"PLAY AGAIN"
+			)
+			)
+		{
+			Application.LoadLevel("Start");
+		}
+	}
+}
---
+++ Luminos Void/Assets/Scripts/StartGuiScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGuiScript : MonoBehaviour 
+{
+	public GUISkin myGuiSkin;
+	public float scaleX = 0.5f;
+	public float scaleY = 0.5f;
+	private bool displayGui = false;
+	private float Timer = 0.0f;
+	
+	void Update ()
+	{
+		Timer += Time.deltaTime;
+
+		if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2")) 
+		{
+			displayGui = true;
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.skin = myGuiSkin;
+		
+		const int buttonWidth = 200;
+		const int buttonHeight = 50;
+
+		if (Timer > 8 || displayGui)
+		{
+			if (
+				GUI.Button(
+				new Rect(
+				((float)Screen.width * scaleX) - (buttonWidth / 2),
+				((float)Screen.height * scaleY) - (buttonHeight / 2),
+				buttonWidth,
+				buttonHeight
+				),
+				"START"
+				)
+				)
+			{
+				Application.LoadLevel("OpeningScene");
+			}
+		}
+	}
+}
