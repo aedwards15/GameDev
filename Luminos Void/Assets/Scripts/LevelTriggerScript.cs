d6ad79cef2ddb6872d7a92cbaa125fa6a9f2ba99
Brian Kovacs <brian.kovacs@me.com>
2014-06-04 05:13:05 -0400

    Added level trigger script and prefab

---
+++ Luminos Void/Assets/Scripts/LevelTriggerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTriggerScript : MonoBehaviour 
+{
+	public string level;
+
+	// Use this for initialization
+	void Start () { }
+	
+	// Update is called once per frame
+	void Update () { }
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		PlayerScript player = otherCollider.gameObject.GetComponent<PlayerScript>();
+		
+		if (player != null)
+		{
+			Application.LoadLevel(level);
+		}
+	}
+}
