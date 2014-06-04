6e5ee484716a871fa95c4672023b31113caf8d59
Brian Kovacs <brian.kovacs@me.com>
2014-06-04 05:02:25 -0400

    Finally got the door script working

---
+++ Luminos Void/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour 
+{
+	private bool allDead;
+
+	void Start () { }
+	
+	void Update () 
+	{
+		allDead = true;
+
+		foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy")) 
+		{
+			if (enemy.collider2D.enabled)
+			{
+				allDead = false;
+			}
+		}
+
+		if (allDead) 
+		{
+			this.gameObject.collider2D.enabled = false;
+		}
+		else
+		{
+			this.gameObject.collider2D.enabled = true;
+		}
+	}
+}
